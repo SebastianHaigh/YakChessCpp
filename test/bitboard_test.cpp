e586568c5543cc0fa0832bba1c0340d1bd517305
SebastianHaigh <sebhaigh27@googlemail.com>
2021-08-08 19:44:24 +0100

    Add tests for bitboard operations
    Tests have been added for north, east, south, and west bitboard
    operations. For each of these operations two test cases are now
    available:
    
    - Using the operations to shift bits from a rank or file in the
    middle of the board. This means the bits will still all be on the
    board after the operation.
    - Using the operations to shift bits from a rank or file at the
    edge of the board. This means that none of the bits will be on the
    board after the operation.

--- test/bitboard_test.cpp
+++ test/bitboard_test.cpp
@@ -1,6 +1,32 @@
 #include <gtest/gtest.h>
-
 #include "../bitboard.h"
+
+TEST(BitBoardTest, SouthOne) {
+  // Assemble
+    Bitboard initial_bitboard = 0x000000000000ff00; // RANK 2
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0x00000000000000ff; // RANK 1
+  // Act
+
+    shifted_bitboard = bitboard::south_one(initial_bitboard);
+
+  // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
+TEST(BitBoardTest, SouthOneFromRank1) {
+  // Assemble
+    Bitboard initial_bitboard = 0x00000000000000ff; // RANK 1
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0; // EMPTY BOARD
+  // Act
+
+    shifted_bitboard = bitboard::south_one(initial_bitboard);
+
+  // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
 
 TEST(BitBoardTest, NorthOne) {
     // Assemble
@@ -12,5 +38,70 @@ TEST(BitBoardTest, NorthOne) {
     shifted_bitboard = bitboard::north_one(initial_bitboard);
     
     // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
+TEST(BitBoardTest, NorthOneFromRank8) {
+  // Assemble
+    Bitboard initial_bitboard = 0xff00000000000000; // RANK 8
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0; // EMPTY BOARD
+  // Act
+
+    shifted_bitboard = bitboard::north_one(initial_bitboard);
+
+  // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
+TEST(BitBoardTest, EastOne) {
+  // Assemble
+    Bitboard initial_bitboard = 0x0202020202020202; // B FILE
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0x0404040404040404; // C FILE
+  // Act
+
+    shifted_bitboard = bitboard::east_one(initial_bitboard);
+
+  // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
+TEST(BitBoardTest, EastOneFromHFile) {
+  // Assemble
+    Bitboard initial_bitboard = 0x8080808080808080; // H FILE
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0; // EMPTY BOARD
+  // Act
+
+    shifted_bitboard = bitboard::east_one(initial_bitboard);
+
+  // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
+TEST(BitBoardTest, WestOne) {
+  // Assemble
+    Bitboard initial_bitboard = 0x0202020202020202; // B FILE
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0x0101010101010101; // A FILE
+  // Act
+
+    shifted_bitboard = bitboard::west_one(initial_bitboard);
+
+  // Assert
+    EXPECT_EQ(shifted_bitboard, expected_bitboard);
+}
+
+TEST(BitBoardTest, WestOneFromAFile) {
+  // Assemble
+    Bitboard initial_bitboard = 0x0101010101010101; // A FILE
+    Bitboard shifted_bitboard = 0;
+    Bitboard expected_bitboard = 0; // EMPTY BOARD
+  // Act
+
+    shifted_bitboard = bitboard::west_one(initial_bitboard);
+
+  // Assert
     EXPECT_EQ(shifted_bitboard, expected_bitboard);
 }
