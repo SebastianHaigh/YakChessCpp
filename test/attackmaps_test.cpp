9511239c635790356d88d6ff848cf2d40dacfb56
SebastianHaigh <sebhaigh27@googlemail.com>
2021-08-09 15:11:53 +0100

    Add new test suite for SouthRay::ray method
    Three tests of SouthRay::ray have been added to
    attackmaps_test.cpp. These are essentially copies of the three
    tests that exist for NorthRay::ray, but they have been adapted
    to test a ray that runs south from a given square. The three
    tests are:
    
    1. CanReturnRayWhenThereAreNoBlockingPieces - tests the
    generation of a south ray extending from A6 (square index 40)
    on an empty board.
    
    2. CanReturnRayWhenThereIsASingleBlockingPiece - tests the
    generation of south ray extending from A6 (square index 40)
    on a board with a single blocking piece on A2 (square index 8).
    
    3. ReturnsEmptyBitboardForAnySquareOnTheFirstRank - tests that
    the SouthRay::ray method returns an empty bit board for any
    square on the 1st rank (A1, B1, C1, ..., H1) (square indices
    0, 1, 2, ..., 7).
    
    The three tests have been added to a new test suite:
    SouthRayAttackMapTest

--- test/attackmaps_test.cpp
+++ test/attackmaps_test.cpp
@@ -75,3 +75,77 @@ TEST(AttackMapTest, ReturnsEmptyBitboardForAnySquareOnTheEightRank) {
     EXPECT_EQ(attack_ray[6], 0);
     EXPECT_EQ(attack_ray[7], 0);
 }
+
+TEST(SouthRayAttackMapTest, CanReturnRayWhenThereAreNoBlockingPieces) {
+    // . . . . . . . . 
+    // . . . . . . . . 
+    // x . . . . . . . 
+    // 1 . . . . . . . 
+    // 1 . . . . . . .
+    // 1 . . . . . . .
+    // 1 . . . . . . .
+    // 1 . . . . . . .
+
+    // Assemble
+    auto ray = attacks::SouthRay();
+    Square serialised_piece = 40;
+    Bitboard occupied_squares = 0;
+    Bitboard attack_ray;
+
+    // Act
+    attack_ray = ray.get(serialised_piece, occupied_squares);
+
+    // Assert
+    std::stack<Square> serialised_ray = bitboard::scan_forward(attack_ray);
+    EXPECT_EQ(serialised_ray.size(), 5);
+}
+
+TEST(SouthRayAttackMapTest, CanReturnRayWhenThereIsASingleBlockingPiece) {
+    // . . . . . . . . 
+    // . . . . . . . . 
+    // x . . . . . . . 
+    // 1 . . . . . . . 
+    // 1 . . . . . . .
+    // 1 . . . . . . .
+    // B . . . . . . .
+    // . . . . . . . .
+
+    // Assemble
+    auto ray = attacks::SouthRay();
+    Square serialised_piece = 40;
+    Bitboard occupied_squares = 1;
+    occupied_squares = occupied_squares << 8;
+    Bitboard attack_ray;
+
+    // Act
+    attack_ray = ray.get(serialised_piece, occupied_squares);
+
+    // Assert
+    std::stack<Square> serialised_ray = bitboard::scan_forward(attack_ray);
+    EXPECT_EQ(serialised_ray.size(), 4);
+    EXPECT_EQ(serialised_ray.top(), 8);
+}
+
+TEST(SouthRayAttackMapTest, ReturnsEmptyBitboardForAnySquareOnTheFirstRank) {
+
+    // Assemble
+    auto ray = attacks::SouthRay();
+    Square serialised_pieces[8] = {0, 1, 2, 3, 4, 5, 6, 7};
+    Bitboard occupied_squares = 0;
+    Bitboard attack_ray[8];
+
+    // Act
+    for (int i = 0; i < 8; i++) {
+        attack_ray[i] = ray.get(serialised_pieces[i], occupied_squares);
+    }
+
+    // Assert
+    EXPECT_EQ(attack_ray[0], 0);
+    EXPECT_EQ(attack_ray[1], 0);
+    EXPECT_EQ(attack_ray[2], 0);
+    EXPECT_EQ(attack_ray[3], 0);
+    EXPECT_EQ(attack_ray[4], 0);
+    EXPECT_EQ(attack_ray[5], 0);
+    EXPECT_EQ(attack_ray[6], 0);
+    EXPECT_EQ(attack_ray[7], 0);
+}
