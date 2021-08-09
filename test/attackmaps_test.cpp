c3203cb5d4dc44f07e3932efbf2cc2d21de77c90
SebastianHaigh <sebhaigh27@googlemail.com>
2021-08-09 14:19:09 +0100

    Add third test for NorthRay::get
    The added test tests that any north ray generated from the eighth
    rank will always be an empty bitboard.

--- test/attackmaps_test.cpp
+++ test/attackmaps_test.cpp
@@ -51,3 +51,27 @@ TEST(AttackMapTest, CanReturnRayWhenThereIsASingleBlockingPiece) {
     EXPECT_EQ(serialised_ray.size(), 4);
     EXPECT_EQ(serialised_ray.top(), 24);
 }
+
+TEST(AttackMapTest, ReturnsEmptyBitboardForAnySquareOnTheEightRank) {
+
+    // Assemble
+    attacks::NorthRay ray = attacks::NorthRay();
+    Square serialised_pieces[8] = {56, 57, 58, 59, 60, 61, 62, 63};
+    Bitboard occupied_squares = 0;
+    Bitboard attack_ray[8];
+
+    // Act
+    for (int i = 0; i < 8; i++) {
+        attack_ray[i] = ray.get(serialised_pieces[i], occupied_squares);
+    }
+
+    // Assert
+    EXPECT_EQ(attack_ray[0], 0);
+    EXPECT_EQ(attack_ray[1], 0);
+    EXPECT_EQ(attack_ray[2], 0);
+    EXPECT_EQ(attack_ray[3], 0);
+    EXPECT_EQ(attack_ray[4], 0);
+    EXPECT_EQ(attack_ray[5], 0);
+    EXPECT_EQ(attack_ray[6], 0);
+    EXPECT_EQ(attack_ray[7], 0);
+}
