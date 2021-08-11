f61fadf6d70739ad23311d2c927ec328386ce7ec
SebastianHaigh <sebhaigh27@googlemail.com>
2021-08-11 13:31:50 +0100

    Implement PseudoLegalMove class

--- movegen.cpp
+++ movegen.cpp
@@ -1,1 +1,48 @@
 #include "movegen.h"
+
+namespace move_generation {
+
+void PseudoLegalMove::execute() {
+    piece_to_move->make_move(source, target);
+    move_made = 1;
+}
+
+void PseudoLegalMove::undo() {
+    if (move_made) {
+        piece_to_move->make_move(source, target);
+        move_made = 0;
+    }
+}
+PseudoLegalMoves MoveGenerator::generate_pseudo_legal_moves() {
+    if (white_to_move()) {
+        return pseudo_legal_moves(*white_pieces, *black_pieces);
+    } else {
+        return pseudo_legal_moves(*black_pieces, *white_pieces);
+    }
+}
+
+PseudoLegalMoves MoveGenerator::pseudo_legal_moves(pieces::ChessMen friendly_pieces, pieces::ChessMen opponent_pieces) {
+    auto pawns = friendly_pieces.get_pawns();
+
+    // Pushes should be good as is. They just need to be serialised and place in 
+    // PseudoLegalMove objects.
+
+    auto single_pushes = pawns.single_pushes();
+    auto double_pushes = pawns.double_pushes();
+
+    // For the captures we need to determine which of the enemy pieces is being 
+    // captured. A way to do this could be add a method to chessmen that return 
+    // a pointer to the piece that is on a given sqaure
+    auto west_captures = pawns.west_captures();
+    auto east_captures = pawns.east_captures();
+
+    return PseudoLegalMoves();
+}
+
+
+bool MoveGenerator::white_to_move() {
+    return side_to_move();
+}
+
+
+} // namespace move_generation
--- movegen.h
+++ movegen.h
@@ -10,10 +10,43 @@ namespace move_generation {
 
 class MoveGenerator {
     private:
-        std::vector<std::unique_ptr<pieces::Pieces>> white_pieces;
-        std::vector<std::unique_ptr<pieces::Pieces>> black_pieces;
+        bool side_to_move = 1;
+        pieces::ChessMen white_pieces;
+        pieces::ChessMen black_pieces;
+        PseudoLegalMoves pseudo_legal_moves(pieces::ChessMen friendly_pieces, pieces::ChessMen opponent_pieces);
+        bool white_to_move();
     public:
-        MoveGenerator();
+        MoveGenerator(pieces::ChessMen white_pieces, 
+            pieces::ChessMen black_pieces) 
+            : white_pieces(white_pieces), black_pieces(black_pieces) {};
+        PseudoLegalMoves generate_pseudo_legal_moves();
+};
+
+class PseudoLegalMoves {
+    public:
+        PseudoLegalMoves();
+};
+
+
+class Move {
+    public:
+        virtual ~Move();
+        virtual void execute() = 0;
+        virtual void undo() = 0;
+};
+
+class PseudoLegalMove : public Move {
+    private:
+        std::shared_ptr<pieces::Piece> piece_to_move;
+        Square source;
+        Square target;
+        bool move_made = 0;
+    public:
+        PseudoLegalMove(std::shared_ptr<pieces::Piece> piece_to_move, 
+            Square source, Square target) 
+            : piece_to_move(piece_to_move), source(source), target(target) {};
+        void execute() override;
+        void undo() override;
 };
 
 } // namespace move_generation
