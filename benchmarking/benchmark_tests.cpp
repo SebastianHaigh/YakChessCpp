ed99804c3e4353d018c47ab7401f8c63d8f13095
SebastianHaigh <sebhaigh27@googlemail.com>
2022-07-01 10:43:07 +0100

    Remove tests for old code.

--- benchmarking/benchmark_tests.cpp
+++ benchmarking/benchmark_tests.cpp
@@ -37,60 +37,10 @@ void PAWN_MOVE_GENERATION() {
 	}
 
 
-	{
-		std::cout << "---------------------------" << std::endl;
-		std::cout << "PAWN_MOVE_GENERATION faster 2" << std::endl;
-		std::cout << "---------------------------" << std::endl;
-		faster::Move move_list[330];
-		std::random_device rand_dev;
-		std::mt19937 generator(rand_dev());
-		std::uniform_int_distribution<int>  distr(0, 1);
-		std::vector<PieceColour> a;
-		int move_counter = 0;
-		for (int i = 0; i < 1000; i++) {
-			a.push_back((PieceColour)distr(generator));
-		}
-		Timer timer;
-		
-		for (int i = 0; i < 1000; i++) {
-			faster::generate_pawn_moves(a[i], &move_list[0], move_counter, bitboard::RANK_2, bitboard::NOT_RANK_2, bitboard::RANK_3);
-		}
-		/*
-		for (int i = 0; i < 30; i++) {
-			std::cout << move_list[i].from << ", " << move_list[i].to << ", " << move_list[i].capture << std::endl;
-		}
-		*/
-		
-	}
-
-
-
 }
 
 void KNIGHT_MOVE_GENERATION() {
-	pieces::StandardPieceFactory factory;
 
-	{
-		pieces::StandardPieceFactory factory;
-		std::unique_ptr<pieces::Piece> knights = factory.make_piece(PieceType::KNIGHT);
-
-		std::cout << "---------------------------" << std::endl;
-		std::cout << "KNIGHT_MOVE_GENERATION" << std::endl;
-		std::cout << "---------------------------" << std::endl;
-		std::vector<std::pair<Square, Square>> moves;
-		Timer timer;
-
-		for (int i = 0; i < 1000; i++) {
-			
-			knights->quiet_moves(bitboard::RANK_2, bitboard::NOT_RANK_2);
-			moves = knights->captures(bitboard::RANK_1, Bitboard(0), bitboard::RANK_3 | bitboard::RANK_4);
-		}
-
-		//for (auto move : moves) {
-		//	std::cout << move.first << ", " << move.second << std::endl;
-		//}
-
-	}
 
 	attacks::KnightAttacks atks;
 	{
@@ -116,82 +66,13 @@ void KNIGHT_MOVE_GENERATION() {
 }
 
 void ROOK_MOVE_GENERATION() {
-	pieces::StandardPieceFactory factory;
 
-	{
-		pieces::StandardPieceFactory factory;
-		std::unique_ptr<pieces::Piece> rook = factory.make_piece(PieceType::ROOK);
 
-		std::cout << "---------------------------" << std::endl;
-		std::cout << "ROOK_MOVE_GENERATION" << std::endl;
-		std::cout << "---------------------------" << std::endl;
-		std::vector<std::pair<Square, Square>> moves;
-		Timer timer;
 
-		for (int i = 0; i < 1000; i++) {
-
-			rook->quiet_moves(bitboard::RANK_2, bitboard::NOT_RANK_2);
-			moves = rook->captures(bitboard::RANK_1, Bitboard(0), bitboard::RANK_3 | bitboard::RANK_4);
-		}
-
-		//for (auto move : moves) {
-		//	std::cout << move.first << ", " << move.second << std::endl;
-		//}
-
-	}
-
-	
-	{
-		std::cout << "---------------------------" << std::endl;
-		std::cout << "ROOK_MOVE_GENERATION fast" << std::endl;
-		std::cout << "---------------------------" << std::endl;
-		faster::Move move_list[330];
-		Bitboard enemy = bitboard::RANK_8;
-		Bitboard friendly = bitboard::RANK_1;
-		Bitboard occupied = enemy | friendly;
-		Bitboard empty = ~occupied;
-		int move_counter = 0;
-		faster::SpecialisedRay<Direction::NORTH> n_ray;
-		faster::SpecialisedRay<Direction::EAST> e_ray;
-		faster::SpecialisedRay<Direction::SOUTH> s_ray;
-		faster::SpecialisedRay<Direction::WEST> w_ray;
-		std::vector<faster::Ray*> rook_atks = { &n_ray, &e_ray, &s_ray, &w_ray };
-		Timer timer;
-
-		for (int i = 0; i < 1000; i++) {
-			generate_sliding_piece_moves(rook_atks, &move_list[0], move_counter, friendly, empty, enemy);
-		}
-
-		//for (int i = 0; i < 50; i++) {
-		//	std::cout << move_list[i].from << ", " << move_list[i].to << ", " << move_list[i].capture << std::endl;
-		//}
-	}
 
 	{
 		std::cout << "---------------------------" << std::endl;
-		std::cout << "ROOK_MOVE_GENERATION fast factory" << std::endl;
-		std::cout << "---------------------------" << std::endl;
-		faster::Move move_list[330];
-		Bitboard enemy = bitboard::RANK_8;
-		Bitboard friendly = bitboard::RANK_1;
-		Bitboard occupied = enemy | friendly;
-		Bitboard empty = ~occupied;
-		int move_counter = 0;
-		faster::RookAttackMap rook_atks;
-		Timer timer;
-
-		for (int i = 0; i < 1000; i++) {
-			generate_sliding_piece_moves(&rook_atks, &move_list[0], move_counter, friendly, empty, enemy);
-		}
-
-		//for (int i = 0; i < 50; i++) {
-		//	std::cout << move_list[i].from << ", " << move_list[i].to << ", " << move_list[i].capture << std::endl;
-		//}
-	}
-
-	{
-		std::cout << "---------------------------" << std::endl;
-		std::cout << "GENERATE ALL MOVES" << std::endl;
+		std::cout << "GENERATE ALL MOVES METAFUNCTIONS" << std::endl;
 		std::cout << "---------------------------" << std::endl;
 		faster::Move move_list[330];
 		Bitboard pawns = bitboard::RANK_2;
@@ -206,23 +87,15 @@ void ROOK_MOVE_GENERATION() {
 		attacks::KnightAttacks knight_atks;
 		attacks::KingAttacks king_atks;
 		int move_counter = 0;
-		faster::SpecialisedRay<Direction::NORTH> n_ray;
-		faster::SpecialisedRay<Direction::EAST> e_ray;
-		faster::SpecialisedRay<Direction::SOUTH> s_ray;
-		faster::SpecialisedRay<Direction::WEST> w_ray;
-		faster::SpecialisedRay<Direction::NORTH_EAST> ne_ray;
-		faster::SpecialisedRay<Direction::SOUTH_EAST> se_ray;
-		faster::SpecialisedRay<Direction::NORTH_WEST> nw_ray;
-		faster::SpecialisedRay<Direction::SOUTH_WEST> sw_ray;
 
-		std::vector<faster::Ray*> rook_atks = { &n_ray, &e_ray, &s_ray, &w_ray };
-		std::vector<faster::Ray*> bishop_atks = { &ne_ray, &se_ray, &nw_ray, &sw_ray };
-		std::vector<faster::Ray*> queen_atks = { &n_ray, &e_ray, &s_ray, &w_ray, &se_ray, &ne_ray, &nw_ray, &sw_ray };
+		faster::RookMap rook_atks;
+		faster::BishopMap bishop_atks;
+		faster::QueenMap queen_atks;
 		Timer timer;
 
 		for (int i = 0; i < 1000; i++) {
 			move_counter = 0;
-			faster::generate_pawn_moves(PieceColour::WHITE, &move_list[0], move_counter, pawns, empty, 0);
+			faster::generate_pawn_moves<PieceColour::WHITE>(&move_list[0], move_counter, pawns, empty, 0);
 			faster::jumping_piece_moves(&knight_atks, &move_list[0], move_counter, knights, empty, 0);
 			faster::jumping_piece_moves(&king_atks, &move_list[0], move_counter, kings, empty, 0);
 			faster::generate_sliding_piece_moves(rook_atks, &move_list[0], move_counter, rooks, empty, 0);
@@ -237,45 +110,6 @@ void ROOK_MOVE_GENERATION() {
 		//}
 	}
 
-	{
-		std::cout << "---------------------------" << std::endl;
-		std::cout << "GENERATE ALL MOVES factory" << std::endl;
-		std::cout << "---------------------------" << std::endl;
-		faster::Move move_list[330];
-		Bitboard pawns = bitboard::RANK_2;
-		Bitboard rooks = bitboard::RANK_1 & (bitboard::FILE_A | bitboard::FILE_H);
-		Bitboard knights = bitboard::RANK_1 & (bitboard::FILE_B | bitboard::FILE_G);
-		Bitboard bishops = bitboard::RANK_1 & (bitboard::FILE_C | bitboard::FILE_F);
-		Bitboard queens = bitboard::RANK_1 & bitboard::FILE_D;
-		Bitboard kings = bitboard::RANK_1 & bitboard::FILE_E;
-		Bitboard friendly = bitboard::RANK_1 | bitboard::RANK_2;
-		Bitboard occupied = friendly;
-		Bitboard empty = ~occupied;
-		attacks::KnightAttacks knight_atks;
-		attacks::KingAttacks king_atks;
-		int move_counter = 0;
-
-		faster::RookAttackMap rook_atks;
-		faster::BishopAttackMap bishop_atks;
-		faster::QueenAttackMap queen_atks;
-		Timer timer;
-
-		for (int i = 0; i < 1000; i++) {
-			move_counter = 0;
-			faster::generate_pawn_moves(PieceColour::WHITE, &move_list[0], move_counter, pawns, empty, 0);
-			faster::jumping_piece_moves(&knight_atks, &move_list[0], move_counter, knights, empty, 0);
-			faster::jumping_piece_moves(&king_atks, &move_list[0], move_counter, kings, empty, 0);
-			faster::generate_sliding_piece_moves(&rook_atks, &move_list[0], move_counter, rooks, empty, 0);
-			faster::generate_sliding_piece_moves(&bishop_atks, &move_list[0], move_counter, bishops, empty, 0);
-			faster::generate_sliding_piece_moves(&queen_atks, &move_list[0], move_counter, queens, empty, 0);
-		}
-
-		std::cout << move_counter << std::endl;
-
-		//for (int i = 0; i < 50; i++) {
-		//	std::cout << move_list[i].from << ", " << move_list[i].to << ", " << move_list[i].capture << std::endl;
-		//}
-	}
-
+	
 	
 }
