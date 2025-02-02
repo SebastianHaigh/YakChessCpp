1744a5177d3640834e67ae54f373640d45fabc0c
Sebastian Haigh <sebhaigh27@googlemail.com>
2025-02-02 18:00:53 +0000

    Add non-benchmarked perft tests and exclude the benchmarks from make test runs

--- src/perft/PerftTests.cpp
+++ src/perft/PerftTests.cpp
@@ -16,6 +16,68 @@ TEST_CASE("Perft Test")
 
   Board board{ "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" };
 
+  SECTION("One Move")
+  {
+    result = perft(board, 1);
+    CHECK(result.m_total == 20);
+    CHECK(result.m_captures == 0);
+  };
+
+  SECTION("Two Moves")
+  {
+    result = perft(board, 2);
+    CHECK(result.m_total == 400);
+    CHECK(result.m_captures == 0);
+  };
+
+  SECTION("Three Moves")
+  {
+    result = perft(board, 3);
+    CHECK(result.m_total == 8902);
+    CHECK(result.m_captures == 34);
+  };
+
+  SECTION("Four Moves")
+  {
+    result = perft(board, 4);
+    CHECK(result.m_total == 197281);
+    CHECK(result.m_captures == 1576);
+  };
+
+  SECTION("Five Moves")
+  {
+    result = perft(board, 5);
+    CHECK(result.m_total == 4865609);
+  };
+
+  SECTION("Six Moves")
+  {
+    result = perft(board, 6);
+    CHECK(result.m_total == 119060324);
+  };
+
+  SECTION("Seven Moves")
+  {
+    result = perft(board, 7);
+    CHECK(result.m_total == 3195901860);
+  };
+
+  // Currently takes too long to run
+  //
+  /* SECTION("Eight Moves") */
+  /* { */
+  /*   result = perft(board, 8); */
+  /*   CHECK(result.m_total == 84998978956); */
+  /* }; */
+
+}
+
+TEST_CASE("Perft Test", "[benchmark]")
+{
+  PerftResult result;
+
+  Board board{ "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" };
+
   BENCHMARK("One Move")
   {
     result = perft(board, 1);
@@ -47,32 +109,20 @@ TEST_CASE("Perft Test")
   BENCHMARK("Five Moves")
   {
     result = perft(board, 5);
-    /* CHECK(result.m_total == 197281); */
+    CHECK(result.m_total == 4865609);
   };
 
   BENCHMARK("Six Moves")
   {
     result = perft(board, 6);
-    /* CHECK(result.m_total == 197281); */
+    CHECK(result.m_total == 119060324);
   };
 
   BENCHMARK("Seven Moves")
   {
     result = perft(board, 7);
-    /* CHECK(result.m_total == 197281); */
+    CHECK(result.m_total == 3195901860);
   };
-
-  BENCHMARK("Eight Moves")
-  {
-    result = perft(board, 8);
-    /* CHECK(result.m_total == 197281); */
-  };
-
-  BENCHMARK("Ten Moves")
-  {
-    result = perft(board, 10);
-  };
-
 }
 
 } // namespace yak
